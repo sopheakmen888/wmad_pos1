a20a1a2ef1b5044210d8bd76c8ca455e870eb55b
vanntoengcaptaincambodia <toeng.vann@institute.pse.ngo>
2025-01-10 14:26:41 +0700

    commit purchase Referent list page about ui and api get

---
+++ app/(protected)/stockin/page-tableview.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { purchaseModel } from "@/models/api/purchaseModel";
+import PaginationData from "@/models/PaginationData";
+import { TableViewPagination } from "@/components/tableview-pagination";
+
+interface Props {
+  title: string;
+  data: PaginationData<purchaseModel>;
+}
+
+export const PageTableView: React.FC<Props> = ({ title, data }) => {
+  const [paginatedData, setPaginatedData] = useState(data);
+
+  const handlePrevClick = () => setPaginatedData((prev) => {
+    return { ...prev, currentPage: data.prevPage };
+  });
+
+  const handleNextClick = () =>
+    setPaginatedData((prev) => {
+      return { ...prev, currentPage: data.nextPage };
+    });
+
+  const handlePageClick = (i: number) => setPaginatedData({ ...paginatedData, currentPage: i + 1 })
+
+  return (
+    <div className="space-y-6">
+      <h1 className="text-3xl font-bold">{title}</h1>
+
+      <div className="flex justify-between items-center">
+        <Input className="max-w-sm" placeholder="Search products..." />
+        <Button>Add Product</Button>
+      </div>
+
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>referenceNumber</TableHead>
+              <TableHead>stockInDate</TableHead>
+              <TableHead>supplierName</TableHead>
+              <TableHead>numberOfProduct</TableHead>
+              <TableHead>purchaseAmount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {paginatedData.records.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.referenceNumber}</TableCell>
+                <TableCell>{item.stockInDate.toLocaleDateString()}</TableCell>
+                <TableCell>{item.supplierName}</TableCell>
+                <TableCell>{item.numberOfProduct}</TableCell>
+                <TableCell>{item.purchaseAmount}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      {/* Pagination */}
+      <TableViewPagination
+        onPrevClick={handlePrevClick}
+        onNextClick={handleNextClick}
+        onPageClick={(i) => handlePageClick(i)}
+        path="/stockin"
+        data={paginatedData}
+      />
+    </div>
+  );
+}
